d847bde1d56a63b505c55d939864a4e7b8341763
Sultan A. Dev <sultanabaniks@gmail.com>
2025-05-29 08:08:38 +0100

    Implement QR code generation and file handling; add silly name and superhero features

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const fs = require("fs");
+import generateName from "sillyname";
+import superheroes, { randomSuperhero } from "superheroes";
+import fs from "fs";
+import { input } from "@inquirer/prompts";
 
-// fs.writeFile("output.txt", "Hello, World!", (err) => {
-//   if (err) {
-//     console.error("Error writing to file:", err);
-//   } else {
-//     console.log("File written successfully!");
-//   }
-// });
+import qr from "qr-image";
 
-fs.readFile("./output.txt", "utf8" ,(err, data) => {
+var sillyName = generateName();
+
+const answer = await input({
+  message: "Enter your website url",
+  required: true,
+});
+
+var qr_svg = qr.image(answer);
+qr_svg.pipe(fs.createWriteStream("qr.png"));
+
+console.log("Your name is:", answer);
+
+console.log("Silly Name:", sillyName);
+console.log("Random Superhero:", randomSuperhero());
+
+fs.writeFile("output.txt", sillyName, (err) => {
   if (err) {
-    console.error("Error reading file:", err);
+    console.error("Error writing to file:", err);
   } else {
-    console.log("File content:", data);
+    console.log("File written successfully!");
+  }
+});
+
+fs.writeFile("output2.txt", answer, (err) => {
+  if (err) {
+    console.error("Error writing to file:", err);
+  } else {
+    console.log("File written successfully!");
   }
 });
