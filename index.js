763a2c3154511401f5a95961113bbfe072009e0f
Sultan A. Dev <sultanabaniks@gmail.com>
2025-06-16 23:37:13 +0100

    chore(update)

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 
 const app = express();
 
-
 app.get("/", (req, res) => {
   res.send({
     message: "Welcome to the Home Page",
@@ -17,6 +16,14 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.send("Contact Page");
+});
+
+app.post("/register", (req, res) => {
+  res.sendStatus(201)
+});
+
+app.put("/user/angela", (req, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(3000, () => {
